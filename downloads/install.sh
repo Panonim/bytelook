15ea808010dc36d692c7a2e9767f46316a504a0c
Artur Flis <office@bluee.dev>
2024-05-28 19:15:00 +0200

    Update install.sh

--- downloads/install.sh
+++ downloads/install.sh
@@ -5,42 +5,37 @@ command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
+# Check if GCC is installed
+if ! command_exists gcc; then
+    echo "GCC is not installed. Please install GCC to proceed."
+    exit 1
+fi
+
 # Check if curl is installed
 if ! command_exists curl; then
-    echo "curl is not installed. Please install curl to proceed with the installation."
+    echo "curl is not installed. Please install curl to proceed."
     exit 1
 fi
 
-# Function to check if a directory is in the PATH
-path_contains() {
-    local target="$1"
-    local path
-    IFS=':' read -ra path <<< "$PATH"
-    for dir in "${path[@]}"; do
-        [[ "$dir" == "$target" ]] && return 0
-    done
-    return 1
-}
+# Download the C source code file
+curl -o bytelook.c https://raw.githubusercontent.com/Panonim/bytelook/main/bytelook.c
 
-# Check if the user has permission to copy to /usr/local/bin
-if [[ ! -w "/usr/local/bin" ]]; then
-    echo "You don't have permission to copy files to /usr/local/bin. Please run this script as root or with sudo."
+# Check if the download was successful
+if [ $? -ne 0 ]; then
+    echo "Failed to download the source code file. Please check your internet connection or try again later."
     exit 1
 fi
 
-# Check if the directory is in the PATH
-if ! path_contains "/usr/local/bin"; then
-    echo "The directory /usr/local/bin is not in your PATH. Please add it to your PATH before proceeding."
+# Compile the C program
+gcc bytelook.c -o bytelook
+
+# Check if compilation was successful
+if [ $? -ne 0 ]; then
+    echo "Failed to compile the source code file. Please check for any compilation errors and try again."
     exit 1
 fi
 
-# Download the compiled binary file
-curl -o compiled https://raw.githubusercontent.com/Panonim/bytelook/main/bytelook.c
+# Move the compiled binary to /usr/local/bin
+sudo mv bytelook /usr/local/bin/ || { echo "Failed to move the binary to /usr/local/bin. Please make sure you have the necessary permissions."; exit 1; }
 
-# Make the binary executable
-chmod +x compiled
-
-# Move the binary to /usr/local/bin
-sudo mv compiled /usr/local/bin
-
-echo "Installation complete! You can now use the 'compiled' command."
+echo "ByteLook has been installed successfully! You can now use the 'bytelook' command."
